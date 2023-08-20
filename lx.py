5e633434c142cc7ec9538df97602bc4f399ccf7c
rakarmp <raka_088@smkassalaambandung.sch.id>
2023-08-20 12:58:52 +0700

    Add Using gTTS for indonesia speak :ocean:

--- lx.py
+++ lx.py
@@ -1,56 +1,56 @@
-import pyttsx3
 import speech_recognition as sr
 import datetime
 import wikipedia
 import webbrowser
 import os
-import smtplib
+from gtts import gTTS
+import playsound
 
 print('Initializing LogicLinx')
 
 MASTER = "Zyarexx"
 
-engine = pyttsx3.init('espeak')
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
-
-# Speak
+# Function to speak
 
 
 def speak(text):
-    engine.say(text)
-    engine.runAndWait()
+    tts = gTTS(text=text, lang='id')
+    tts.save("output.mp3")
+    playsound.playsound("output.mp3")
+    os.remove("output.mp3")
 
 # Function to wish user
 
 
 def wishMe():
-    hour = int(datetime.datetime.now().hour)
+    hour = datetime.datetime.now().hour
     if 0 <= hour < 12:
-        speak("Good Morning" + MASTER)
+        speak("Selamat pagi" + MASTER)
     elif 12 <= hour < 18:
-        speak("Good Afternoon" + MASTER)
+        speak("Selamat siang" + MASTER)
     else:
-        speak("Good Evening" + MASTER)
-    speak("How can I assist you today?")
+        speak("Selamat malam" + MASTER)
+    speak("Ada yang bisa saya bantu?")
 
 # Function to take user command
 
 
 def takeCommand():
     r = sr.Recognizer()
+    # Use the appropriate device index
+    microphone = sr.Microphone(device_index=0)
 
-    with sr.Microphone() as source:
-        print("Listening...")
+    with microphone as source:
+        print("Mendengarkan...")
         audio = r.listen(source)
 
     try:
-        print("Recognizing...")
-        query = r.recognize_google(audio, language="en-us")
-        print(f"User said: {query}\n")
+        print("Mengenali...")
+        query = r.recognize_google(audio, language="id-ID")
+        print(f"Pengguna berkata: {query}\n")
 
     except Exception as e:
-        print("Say that again, please...")
+        print("Tolong ulangi...")
         query = None
 
     return query
@@ -58,33 +58,33 @@ def takeCommand():
 
 # Main program
 if __name__ == "__main__":
-    print("Hello, my name is LogicLinx.")
+    print("Halo, nama saya LogicLinx.")
     wishMe()
 
     while True:
         query = takeCommand().lower()
 
         if 'wikipedia' in query:
-            speak('Searching Wikipedia...')
+            speak('Mencari di Wikipedia...')
             query = query.replace("wikipedia", "")
             results = wikipedia.summary(query, sentences=2)
             print(results)
             speak(results)
 
-        elif 'open youtube' in query:
+        elif 'buka youtube' in query:
             webbrowser.open("https://www.youtube.com")
 
-        elif 'open google' in query:
+        elif 'buka google' in query:
             webbrowser.open("https://www.google.com")
 
-        elif 'play music' in query:
-            # Add your music play logic here
+        elif 'putar musik' in query:
+            # Tambahkan logika untuk memutar musik di sini
             pass
 
-        elif 'what is the time' in query:
+        elif 'jam berapa' in query:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")
-            speak(f"The time is {strTime}")
+            speak(f"Jam sekarang {strTime}")
 
-        elif 'quit' in query or 'exit' in query:
-            speak("Goodbye!")
+        elif 'keluar' in query or 'tutup' in query:
+            speak("Sampai jumpa!")
             break
